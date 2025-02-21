25d0dd3089ea5f74997b983da6a239036a4233d0
Sparsh <gsparsh17@gmail.com>
2025-02-21 17:33:08 +0530

    commit

--- Server/server.js
+++ Server/server.js
@@ -50,12 +50,12 @@ const PORT = process.env.PORT || 5000;
     console.error('Could not connect to MongoDB...', err);
   });
 
-mongoose.connect('mongodb+srv://gsparsh17:Gsparsh17@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected successfully!'))
-.catch(err => console.log('MongoDB connection error: ', err));
+// mongoose.connect('mongodb+srv://gsparsh17:Gsparsh17@cluster0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected successfully!'))
+// .catch(err => console.log('MongoDB connection error: ', err));
 
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -70,19 +70,19 @@ mongoose.connect('mongodb+srv://gsparsh17:Gsparsh17@cluster0.mongodb.net/myFirst
 //   }
 // });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 
 
