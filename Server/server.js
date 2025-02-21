8c470f77ca9a37ab32a8b65c52ecfd690dc7c280
Sparsh <gsparsh17@gmail.com>
2025-02-22 01:47:54 +0530

    commit

--- Server/server.js
+++ Server/server.js
@@ -34,8 +34,6 @@ const upload = multer({ storage: storage });
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(async () => {
     console.log('Connected to MongoDB...');
 
